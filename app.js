87ed11af480a365b767de637ce45a5392388037d
Blake Knight <hrhblakeknight@gmail.com>
2024-05-12 20:46:35 -0700

    installed nodemailer

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(express.static("public"));
 
-// app.post('/mail', (req, res) => {
-//   console.log("mail button clicked");
-// });
+app.post('/mail', (req, res) => {
+  console.log("mail button clicked");
+});
 
 app.listen(port, () => {
   console.log(process.env.SENSITIVE_INFO);
