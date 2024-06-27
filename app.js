b942b8dc3a6d5eca6e5e8f135fdd770f9a147250
Blake Knight <hrhblakeknight@gmail.com>
2024-06-27 15:18:48 -0700

    Implented cors

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import nodemailer from 'nodemailer';
 import * as utils from "./utils/utils.js";
-dotenv.config();
 import * as db from "./utils/database.js";
+import cors from 'cors'; // Import CORS
+
+dotenv.config();
+
 let projects = [];
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(cors()); // Enable CORS
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -21,7 +26,7 @@ app.get("/", async (req, res) => {
     projects = await db.getAllProjects();
     console.log(projects);
     res.render("index.ejs");
-})
+    })
 });
 
 app.get("/projects", async (req, res) => {
@@ -82,7 +87,75 @@ app.get('/test-mail', (req, res) => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).send("This developer is an IDIOT and hasn't coded this page yet!");
+    res.status(404).send(`
+        <html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100vh;
+                        margin: 0;
+                        background-color: #f0f0f0;
+                    }
+                    h1 {
+                        font-size: 2em;
+                        text-align: center;
+                        color: #333;
+                        animation: shake 0.5s infinite;
+                    }
+                    @keyframes shake {
+                        0% { transform: translate(1px, 1px) rotate(0deg); }
+                        10% { transform: translate(-1px, -2px) rotate(-1deg); }
+                        20% { transform: translate(-3px, 0px) rotate(1deg); }
+                        30% { transform: translate(3px, 2px) rotate(0deg); }
+                        40% { transform: translate(1px, -1px) rotate(1deg); }
+                        50% { transform: translate(-1px, 2px) rotate(-1deg); }
+                        60% { transform: translate(-3px, 1px) rotate(0deg); }
+                        70% { transform: translate(3px, 1px) rotate(-1deg); }
+                        80% { transform: translate(-1px, -1px) rotate(1deg); }
+                        90% { transform: translate(1px, 2px) rotate(0deg); }
+                        100% { transform: translate(1px, -2px) rotate(-1deg); }
+                    }
+                    .letter {
+                        display: inline-block;
+                        transition: transform 0.3s ease-out;
+                    }
+                    .falling {
+                        transform: translateY(100vh);
+                    }
+                </style>
+            </head>
+            <body>
+                <h1 id="error-message">This developer is an IDIOT and hasn't coded this page yet!</h1>
+                <script>
+                    const message = document.getElementById('error-message');
+                    const letters = message.textContent.split('');
+                    message.innerHTML = '';
+                    letters.forEach(letter => {
+                        const span = document.createElement('span');
+                        if (letter === ' ') {
+                            span.innerHTML = '&nbsp;';
+                        } else {
+                            span.textContent = letter;
+                        }
+                        span.classList.add('letter');
+                        message.appendChild(span);
+                    });
+                    message.addEventListener('click', () => {
+                        const spans = document.querySelectorAll('.letter');
+                        spans.forEach((span, index) => {
+                            setTimeout(() => {
+                                span.classList.add('falling');
+                            }, index * 100);
+                        });
+                    });
+                </script>
+            </body>
+        </html>
+    `);
 });
 
 app.use((err, req, res, next) => {
